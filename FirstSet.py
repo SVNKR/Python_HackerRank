62a4ca79b286a8ef9b68b7a10630f8b9913f8573
submukherjee <Subhankar.Mukherjee@lexmark.com>
2018-11-22 19:37:28 +0530

    adding instructions to firstSet soln

--- FirstSet.py
+++ FirstSet.py
@@ -1,5 +1,11 @@
 '''
 https://www.hackerrank.com/challenges/symmetric-difference/problem
+^ symbol can also be used to compute the symmetric difference of two sets.
+# Enter your code here. Read input from STDIN. Print output to STDOU
+M = raw_input();m = raw_input().split()
+N = raw_input();n = raw_input().split()
+
+print "\n".join(sorted(list(set(m) ^ set(n)),key=int))
 '''
 
 
